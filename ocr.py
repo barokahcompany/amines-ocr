7da7a0809563cf0084d918f408e3c105d80ac8a5
barakucrut <42163187+Candra06@users.noreply.github.com>
2025-08-05 22:47:05 +0700

    update python API

--- ocr.py
+++ ocr.py
@@ -18,7 +18,7 @@ image_path = data.get("image", "-")
 ocr = PaddleOCR(
     use_textline_orientation=False,  # Disable angle detection (faster)
     text_det_box_thresh=0.8,  # Adjust detection threshold
-    rec_batch_num=4,  # Reduce batch size
+    text_recognition_batch_size=4,  # Reduce batch size
     use_gpu=True,
     lang="en"
     )
