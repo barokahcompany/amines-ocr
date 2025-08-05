ff10bc969d30d163efe1e2e8e52015195d5491b2
barakucrut <42163187+Candra06@users.noreply.github.com>
2025-08-05 19:30:43 +0700

    fix(server) : set up running

--- server.js
+++ server.js
@@ -147,6 +147,6 @@ app.post("/scan-nik", upload.single("ktp"), async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`API berjalan di http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0',() => {
+  console.log(`API berjalan di http://0.0.0.0:${PORT}`);
 });
