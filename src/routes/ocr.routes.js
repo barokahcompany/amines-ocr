bb719afd968def6065a4285f1cb8e1f63fb3f93a
barakucrut <42163187+Candra06@users.noreply.github.com>
2025-08-19 05:55:15 +0700

    fix(db check) : check from db

--- src/routes/ocr.routes.js
+++ src/routes/ocr.routes.js
@@ -1,15 +1,29 @@
-import { Router } from 'express';
+import {
+  Router
+} from 'express';
 import multer from 'multer';
 import path from 'path';
 import fs from 'fs/promises';
-import { ktpOcr } from '../services/ktp-ocr.service.js';
-
+import {
+  ktpOcr
+} from '../services/ktp-ocr.service.js';
+import mysql from "mysql2/promise"
+import 'dotenv/config';
 const router = Router();
+const dbConfig = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+};
+const pool = mysql.createPool(dbConfig);
 
 // simpan file upload sementara
 const upload = multer({
   dest: path.join(process.cwd(), 'uploads'),
-  limits: { fileSize: 2 * 1024 * 1024 }, // 6MB
+  limits: {
+    fileSize: 2 * 1024 * 1024
+  }, // 6MB
   fileFilter: (req, file, cb) => {
     if (!/image\/(png|jpeg|webp)/.test(file.mimetype)) {
       return cb(new Error('File harus berupa PNG/JPG/WEBP'));
@@ -20,15 +34,54 @@ const upload = multer({
 
 router.post('/ktp', upload.single('ktp'), async (req, res, next) => {
   const startedAt = Date.now();
-  if (!req.file) return res.status(400).json({ message: 'Field "ktp" wajib' });
+
+  function hasValue(value) {
+    return typeof value === 'string' && value.trim().length > 0;
+  }
+
+  function normalizeDate(dateStr) {
+    const [d, m, y] = dateStr.split('|');
+    const dd = String(d).padStart(2, '0');
+    const mm = String(m).padStart(2, '0');
+    return `${y}-${mm}-${dd}`;
+  }
+
+  function formatKtpRtRw(input) {
+    const num = parseInt(input, 10);
+    if (isNaN(num)) return '000'; // fallback jika input tidak valid
+    return num.toString().padStart(3, '0');
+  }
+  if (!req.file) return res.status(400).json({
+    message: 'Field "ktp" wajib'
+  });
 
   try {
     const result = await ktpOcr(req.file.path);
     // bersihkan file temp
     await fs.unlink(req.file.path).catch(() => {});
+    let tmpData = result.data;
+    if (tmpData.nik.length == 16) {
+      const nik = result.data.nik;
+      const [rows] = await pool.query(
+        "SELECT * FROM dpt WHERE nik = ?",
+        [nik]
+      );
+      if (rows.length) {
+        const dbData = rows[0];
+        tmpData = {
+          ...tmpData,
+          alamat: hasValue(tmpData.alamat) ? tmpData.alamat : dbData.alamat,
+          tempat_lahir: hasValue(tmpData.tempat_lahir) ? tmpData.tempat_lahir : dbData.tempat_lahir,
+          tanggal_lahir: hasValue(tmpData.tanggal_lahir) ? tmpData.tanggal_lahir : normalizeDate(dbData.tempat_lahir),
+          jenis_kelamin: hasValue(tmpData.jenis_kelamin) ? tmpData.jenis_kelamin : (dbData.jenis_kelamin == "L" ? "LAKI-LAKI" : "PEREMPUAN"),
+          rt: hasValue(tmpData.rt) ? tmpData.rt : formatKtpRtRw(String(dbData.rt)),
+          rw: hasValue(tmpData.rw) ? tmpData.rw : formatKtpRtRw(String(dbData.rw)),
+        }
+      }
+    }
     res.json({
       status: true,
-      data: result.data,
+      data: tmpData,
       meta: {
         model: result.model,
         elapsed_ms: Date.now() - startedAt,
